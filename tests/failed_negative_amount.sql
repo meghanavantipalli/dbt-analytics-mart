20d6a5ba31feb0c982bfc353cc6015afeb485319
meghanavantipalli@gmail.com <meghanavantipalli@gmail.com>
2026-08-12 17:37:30 +0000

    added new test to check the monthly_price for subscription data

--- tests/failed_negative_amount.sql
+++ tests/failed_negative_amount.sql
@@ -1,2 +1,5 @@
-"failed transactoins should not have +ve amount"
-select * from {{ref('stg_transactions')}} where payment_status='failed' and amount>0
+
+select * 
+from {{ ref('stg_transactions') }} 
+where payment_status = 'failed' 
+  and amount > 0
--- tests/success_positive_amounts.sql
+++ tests/success_positive_amounts.sql
@@ -1,2 +1,2 @@
-"#success status should not contain negative amounts"
+
 select * from {{ref('stg_transactions')}} where payment_status='success' and amount<0
